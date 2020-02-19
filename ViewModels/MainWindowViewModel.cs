3a1f6fed2df5e5b56731703f6e66085e214d2a2c
Nex-Coder <callum.cunha@gmail.com>
2020-02-19 03:11:07 +0000

    Refresh

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversalLauncher.Views;
+
+namespace UniversalLauncher.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Blah";
+
+
+
+        // I cant belive i got it to work lmao.
+        public static void CloseWin(Window window) {
+            window.Close();
+        }
+    }
+}
